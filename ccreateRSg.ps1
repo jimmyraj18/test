13872db325f1b5b0bd1a6421b2df50abc99a49d7
jimmyraj18 <jimmysdm0711@gmail.com>
2021-02-09 22:31:14 -0500

    Add files via upload

---
+++ ccreateRSg.ps1
@@ -0,0 +1,7 @@
+﻿#Provide the subscription Id
+$subscriptionId = 'ze1e2-p3d3-DBaaS-01_sbc'
+
+#Provide the name of your resource group
+$resourceGroupName ='AzureBackupRG_northeurope_1'
+
+New-AzureRmResourceGroup -Name $resourceGroupName -Location "South Central US"
